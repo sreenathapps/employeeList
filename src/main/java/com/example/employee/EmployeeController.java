ab80d1953a05dbcadbd3d933ed99844b2d9a27d9
sai <saisreenath.mandaloju@gmail.com>
2024-07-05 15:38:09 +0530

    final Commit

--- src/main/java/com/example/employee/EmployeeController.java
+++ src/main/java/com/example/employee/EmployeeController.java
@@ -7,14 +7,48 @@
  */
 
 // Write your code here
-import org.springframework.web.bind.annotation.*;
 package com.example.employee;
+import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.*;
+import com.example.employee.*;
 /**
  * EmployeeController
  */
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
 @RestController
 public class EmployeeController {
+    EmployeeService employeeService = new EmployeeService();
+    
+    @GetMapping("/employees")
+    public ArrayList<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeById(@PathVariable("employeeId") int employeeId) {
+        return employeeService.getEmployeeById(employeeId);
+    }
+    @PostMapping("/employees")
+    public Employee addEmployees(@RequestBody Employee employee) {
+        return employeeService.addEmployee(employee);
+    }
+
+    @PutMapping("employees/{id}")
+    public Employee updateEmployee(@PathVariable("id") int id, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(id, employee);
+    }
+    @DeleteMapping("employees/{employeeId}")
+    public void deleteEmployee(@PathVariable("employeeId") int employeeId ) {
+        employeeService.deleteEmployee(employeeId);
+    }
     
     
 }
